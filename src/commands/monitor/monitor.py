39ea62891d831f6313c5e1ecb95ec06f1a80e658
Raphaël "Zerka" H <contact.zerka@gmail.com>
2023-08-10 13:12:37 +0000

    :sparkles: Add `kuma monitor pause`

--- src/commands/monitor/monitor.py
+++ src/commands/monitor/monitor.py
@@ -1,4 +1,4 @@
-from . import add, ls, delete
+from . import add, ls, delete, pause
 
 
 def add_subparser(subparsers):
@@ -15,6 +15,7 @@ def add_subparser(subparsers):
     add.monitor_parser(monitor_subparsers)
     ls.monitor_parser(monitor_subparsers)
     delete.monitor_parser(monitor_subparsers)
+    pause.monitor_parser(monitor_subparsers)
 
     # Default action when no subcommand is provided
     monitor_parser.set_defaults(func=lambda _: monitor_parser.print_help())
